2db9baa82fdb3f7759a45e004854e91e2679a954
Ravi Yadav <shortanswer12345@gmail.com>
2022-09-29 19:48:51 +0530

    complete all schemas

---
+++ server/dataBase/AllModels.js
@@ -0,0 +1,17 @@
+import { FoodeModel } from "./Food"
+import { ImageModel } from "./Images"
+import { MenuModel } from "./Menu"
+import { OrderModel } from "./Order"
+import { RestaurantModel } from "./Restaurant"
+import { ReviewModel } from "./Review"
+import { UserModel } from "./User"
+
+export {
+    FoodeModel,
+    ImageModel,
+    MenuModel,
+    OrderModel,
+    RestaurantModel,
+    ReviewModel,
+    UserModel,
+}
